5964a00b50dfbe20da280af5169480c42139d4e3
wonseok <wonseok@tpmn.io>
2022-09-07 16:45:29 +0900

    create vue cli project

---
+++ cli-template/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+<!--  <img alt="Vue logo" src="./assets/logo.png">-->
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+//ES6 방식 -> import 하여 컴포넌트 내용을 넘겨받게함
+//ES5 방식은 객체로 컴포넌트 내용을 담아 넘기는 방식이였다.
+import TodoHeader from "@/components/TodoHeader";
+import TodoInput from "@/components/TodoInput";
+import TodoList from "@/components/TodoList";
+import TodoFooter from "@/components/TodoFooter";
+
+export default {
+  name: 'App',
+  components: {
+    'TodoHeader' : TodoHeader,
+    'TodoInput' : TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter' : TodoFooter,
+  }
+}
+</script>
+
+<style>
+  body {
+    text-align: center;
+    background-color: #F6F6F8;
+  }
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+  button {
+    border-style: groove;
+  }
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0,0,0,0.03);
+  }
+</style>
---
+++ cli-template/src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "TodoHeader"
+}
+</script>
+
+<style scoped> /*scoped 속성은 vue 에서 지원하는 속성으로 해당 스타일은 이 컴포넌트에서만 사용하겠다는 의미이다.*/
+  h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
+</style>
